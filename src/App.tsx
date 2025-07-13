650e0eb52ccabf07199141e0c4c3d48b728ac719
Gpocas <gpocas01@gmail.com>
2025-07-13 13:56:00 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Home } from '@/islands/Home'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='font-bold text-gray-800 text-2xl flex items-center justify-center m-4'>
+        Grade de Issues
+      </h1>
+      <div className='m-4'>
+      <Home />
+      </div>
+    </>
+  )
+}
+
+export default App
